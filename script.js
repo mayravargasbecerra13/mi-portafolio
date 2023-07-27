4cabff648620aeb19c54db58980d695332c9278e
mayravargasbecerra13 <vargasmayra800@gmail.com>
2023-07-27 10:24:24 -0500

    Se agrega portafolio

---
+++ script.js
@@ -0,0 +1,41 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  //oculto el menu una vez que selecciono una opcion
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
+
+//Función que aplica las animaciones de las habilidades
+function efectoHabildades() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[0].classList.add("htmlcss");
+    habilidades[1].classList.add("photoshop");
+    habilidades[2].classList.add("ilustrator");
+    habilidades[3].classList.add("wordpress");
+    habilidades[4].classList.add("drupal");
+    habilidades[5].classList.add("comunicacion");
+    habilidades[6].classList.add("trabajo");
+    habilidades[7].classList.add("creatividad");
+    habilidades[8].classList.add("dedicacion");
+    habilidades[9].classList.add("colaboracion");
+  }
+}
+//detecto el scrolling para aplicar la animación de la barra de habilidades
+window.onscroll = function () {
+  efectoHabildades();
+};
